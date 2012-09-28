95ebb4ecf18a5456b91ad0ec059714b7e227b4e0
christer7 <christer.evervall@gmail.com>
2012-09-28 13:16:49 +0200

    Har ändrat void till long

--- eclipse/src/com/example/mysecondapp/newjavaclass.java
+++ eclipse/src/com/example/mysecondapp/newjavaclass.java
@@ -5,10 +5,15 @@ public class newjavaclass {
 	
 	
 	
+<<<<<<< HEAD
 	int myint = 5;
 	
 	private int mymethod() {
 		return myint;
+=======
+	void long mymethod() {
+		return 0;
+>>>>>>> Har ändrat void till long
 	}
 
 }
